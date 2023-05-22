438c9a9f5835e3ad39fa86ca2f0ce2a7bd447a46
theo <theo.macris@gmail.com>
2023-05-22 16:19:32 -0400

    16

---
+++ 16/main.py
@@ -0,0 +1,24 @@
+# import turtle
+# from turtle import Turtle, Screen
+# import another_module
+#
+# #print(another_module.another_variable)
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(200)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
+
---
+++ 16/oop-coffee-machine-start/main.py
@@ -0,0 +1,56 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# Define menu items
+
+my_cm = CoffeeMaker()
+my_menu = Menu()
+my_mm = MoneyMachine()
+
+def get_order():
+    order = input("What would you like? (espresso/latte/cappuccino):")
+    if order == "off":
+        exit()
+    elif order == "report":
+        my_cm.report()
+        my_mm.report()
+        return False
+        
+    elif order not in my_menu.get_items():
+        print("Incorrect order")
+        return
+    
+    else:
+        return order
+
+def make_coffee(order):
+    drink = my_menu.find_drink(order)
+    has_money = check_money(drink)
+    if not my_cm.is_resource_sufficient(drink):
+        return
+    if not has_money:
+        print("Sorry that's not enough money. Money refunded.")
+        return
+
+    else:
+        my_cm.make_coffee(drink)
+
+def check_money(mydrink):
+    print(mydrink)
+    cost = mydrink.cost
+    print(f"Your {mydrink.name} will cost: ${cost}")
+    if my_mm.make_payment(cost):
+        return True
+    else:
+        return False
+
+
+while True:
+    myorder = get_order()
+    if myorder:
+        make_coffee(myorder)
+
+
+
+
